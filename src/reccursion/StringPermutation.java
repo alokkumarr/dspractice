825c44a819b32b871aa315360dd7b5151abab114
alokkumar <kumar.alok.iet@gmail.com>
2021-09-23 08:53:25 +0530

    New DS & algorithm Problem

---
+++ src/reccursion/StringPermutation.java
@@ -0,0 +1,28 @@
+package reccursion;
+
+public class StringPermutation {
+    public static void main(String[] args) {
+        String s1 = "ABCD";
+        permute(s1, 0, s1.length()-1);
+    }
+
+    static private void permute(String str, int l, int r) {
+        if (l == r)
+            System.out.print(str + ",");
+        else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str,l,i);
+                permute(str, l+1, r);
+                str = swap(str,l,i);
+            }
+        }
+    }
+
+    static String swap(String str, int l, int r) {
+        char[] chars = str.toCharArray();
+        char tmp = chars[l];
+        chars[l] = chars[r];
+        chars[r] = tmp;
+        return String.valueOf(chars);
+    }
+}
